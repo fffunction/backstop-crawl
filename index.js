ff41b170992ff345703dc3713b77d2c7e09e232e
Dan Reeves <heydanreeves@gmail.com>
2017-02-11 21:44:43 +0000

    implemented default limit

--- index.js
+++ index.js
@@ -12,26 +12,33 @@ const cli = meow(`
 
     Options
       --outfile, -o        Save the backstop config to this file
+      --debug              Logs out errors produced while crawling
       --ignore-robots      Ignore the sites robots.txt
       --ignore-ssl-errors  Treat any certificate as valid (e.g. self-signed
                             or expired)
-      --debug              Logs out errors produced while crawling
       --allow-subdomains   Allow crawling links found to subdomains of the
                             current domain
+      --limit-similar[=3]  Limits the number of similar URLs to a set number
+                           Defaults to 3
+                            e.g /blog/1, /blog/2, /blog/3
 
     Examples
       $ backstop-crawl http://localhost
-`, {
-    alias: {
-        o: 'outfile',
-    },
-});
+`,
+    {
+        alias: {
+            o: 'outfile',
+        },
+    });
+
+// Set default if true
+cli.flags.limitSimilar = (cli.flags.limitSimilar) ? 3 : false;
 
 if (cli.input.length) {
     if (validurl(cli.input[0])) {
         crawl(cli.input[0], cli.flags);
     } else {
-        console.error(`Error: "${cli.input[0]}" isn't a valid URL`);
+        console.error(`> Error: "${cli.input[0]}" isn't a valid URL`);
         process.exit(1);
     }
 } else {
